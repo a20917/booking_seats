5df687b95ef521fcce8f2158108633d500601a8b
Nick Lai <a20917@hotmail.com>
2019-07-13 04:34:22 +0800

    1. Implement edit, update and delete for booking 2. Only owner can delete his/her booking 3. Check if the desk and date is conflictive when create and update

--- Align_test/app/controllers/bookings_controller.rb
+++ Align_test/app/controllers/bookings_controller.rb
@@ -1,5 +1,8 @@
 class BookingsController < ApplicationController
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :update]
+  before_action :find_booking_and_check_permission, only: [:edit, :destroy, :update]
+  include BookingsHelper
+
   def index
     @bookings = Booking.all
   end
@@ -8,29 +11,76 @@ class BookingsController < ApplicationController
     @booking = Booking.find(params[:id])
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+    @seats = seat_list
+  end
+
   def new
     @booking = Booking.new
-    @seats = []
-    ['A','B','C'].each do |x|
-      [*1..10].each do |y|
-        @seats.push x + y.to_s
-      end
-    end
+    @seats = seat_list
   end
 
   def create
     @booking = Booking.new(booking_params)
+    unless avalible_check?(booking_params)
+      flash[:alert] = invalid_msg(booking_params)
+      redirect_to new_booking_path
+      return
+    end
+
     @booking.user_id = current_user[:id]
     if @booking.save
       redirect_to bookings_path
     else
-      render :new
+      redirect_to new_booking_path
     end
+  end
+
+  def update
+    unless avalible_check?(booking_params, true)
+      flash[:alert] = invalid_msg(booking_params)
+      redirect_to edit_booking_path(@booking)
+      return
+    end
+
+    if @booking.update(booking_params)
+      redirect_to bookings_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    flash[:alert] = "Booking deleted"
+    redirect_to bookings_path
   end
 
   private
 
+  def find_booking_and_check_permission
+    @booking = Booking.find(params[:id])
+    if current_user != @booking.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
+
   def booking_params
-    params.require(:booking).permit(:name, :desk, :date_from, :date_to)
+    params.require(:booking).permit(:desk, :date_from, :date_to)
+  end
+
+  def avalible_check?(booking_params, self_check = false)
+    bookings = Booking.where("desk = ? AND ((? BETWEEN date_from AND date_to) OR (? BETWEEN date_from AND date_to))",
+                             booking_params[:desk], booking_params[:date_from],
+                             booking_params[:date_to])
+    bookings = bookings.where("id <> ?", params.require(:id)) if self_check
+    return false if bookings.count > 0
+    true
+  end
+
+  def invalid_msg(paras)
+    "The desk [#{paras[:desk]}] is conflicted with other booking " +
+    "during [#{paras[:date_from]} - #{paras[:date_to]}]"
   end
 end
--- Align_test/app/helpers/bookings_helper.rb
+++ Align_test/app/helpers/bookings_helper.rb
@@ -1,2 +1,11 @@
 module BookingsHelper
+  def seat_list
+    seats = []
+    ['A','B','C'].each do |x|
+      [*1..10].each do |y|
+        seats.push x + y.to_s
+      end
+    end
+    seats
+  end
 end
